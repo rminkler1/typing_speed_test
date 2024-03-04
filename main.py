cf539398ea5be4a18f7ae1f5dc52f2009f8f17cc
robertminkler <robertminkler@gmail.com>
2024-03-04 11:36:28 -0700

    add main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter
+
+#Constants
+APP_NAME = "Typing Speed Test"
+
+
+# open tkinter window
+window = tkinter.Tk()
+window.title(APP_NAME)
+window.minsize(width=1000, height=600)
+window.config(padx=20, pady=20)
+
+# TODO: UI setup List of words to type
+
+# TODO: UI setup input box for text input
+
+# TODO: UI setup show results
+
+# TODO: Generate words to type from a list of common English words
+
+# TODO: Compare current word typed with current word displayed & Show errors (RED CHARACTERS)
+
+# TODO: Track time from start of typing to end of test.
+
+# TODO: Calculate CPM, WPM, & Accuracy
+
+
+# keep UI Open
+window.mainloop()
